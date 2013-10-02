42df24c5773e7b0c47da3d02b01ce6860bfef477
Ivan Dymkov <mstrdymio@gmail.com>
2013-10-03 04:09:01 +0800

    Version 0.9

--- jquery.popapilus.js
+++ jquery.popapilus.js
@@ -1,14 +1,49 @@
-// Popapilus v0.8 - jQuery plugin for creating popups: modal windows, hints etc.
-// sources, description and demos - on http://github.com/dymio/popapilus
-// Author: Ivan Dymkov aka Dymio
-// Site: http://dymio.net
-// Email: mstrdymio[at]gmail.com
-(function() {
+/*!
+ *  Popapilus v.0.9 - 2013-10-03
+ *  jQuery plugin for creating popups: modal windows, hints etc.
+ *  http://github.com/dymio/popapilus
+ *  (c) 2013 Ivan Dymkov ( http://dymio.net , @dymio )
+ *  license: http://www.opensource.org/licenses/mit-license.php
+ *
+ *  requires jQuery 1.7.1 or higher
+ */
 
-  /*
-   * Class for overlay
-   */
-  function PopapilusOverlay(options) {
+(function ( $, document, window ) {
+
+  // Plugin bridge
+  $.popapilus = function( options ) {
+    var opts = $.extend( {}, $.popapilus.defaults, options );
+    return new Popapilus( opts );
+  };
+
+  // Plugin defaults
+  $.popapilus.defaults = {
+    css_class: 'popapilus', // can't change after init
+
+    no_overlay:                   false,               // can't change after init
+    overlay_css_class:            'popapilus_overlay', // can't change after init
+    overlay_z_index:              3998,                // can't change after init
+    overlay_show_animation_speed: 0,
+    close_on_overlay_click:       true,
+
+    fixed:    true,  // fixed or absolute position of block
+    z_index:  3999,
+    modal:    true,  // works only if no_overlay == false
+    centered: true,  // always works by x axis but by y axis only if fixed
+    top:      null,  // don't works if centered && fixed
+    right:    null,  // works if not centered
+    bottom:   null,  // don't works if centered && fixed
+    left:     null,  // works if not centered
+    width:    null,
+    height:   null,
+
+    ignore_close_btn: false,
+    show_animation_speed: 0,
+    autoclose_time: 0
+  };
+
+  /** Overlay worker **/
+  function PopapilusOverlay( options ) {
     var self = this;
 
     var overlay = null;
@@ -16,16 +51,15 @@
 
     this.onClickFunction = null;
 
-    this.show = function(show_options) {
-      if (_is_visible) return false;
-      show_options = show_options || options;
-      overlay.fadeIn(show_options.overlay_show_animation_speed);
+    this.show = function( show_options ) {
+      if ( _is_visible ) return false;
+      var opts = $.extend( {}, options, show_options );
+      overlay.fadeIn( opts.overlay_show_animation_speed );
       _is_visible = true;
     }
+
     this.hide = function() {
-      if (!_is_visible) return false;
-      //show_options = show_options || options;
-      //overlay.fadeOut(show_options.overlay_show_animation_speed);
+      if ( !_is_visible ) return false;
       overlay.hide();
       _is_visible = false;
     }
@@ -36,184 +70,137 @@
       overlay = $("body ." + options.overlay_css_class + ":last");
       
       overlay.css({
-        display: 'none',
+        display:  'none',
         position: 'fixed',
-        top: 0,
+        top:  0,
         left: 0,
-        width: '100%',
+        width:  '100%',
         height: '100%',
         'z-index': options.overlay_z_index
       });
 
       overlay.click(function(evnt) {
         evnt.preventDefault();
-        if (self.onClickFunction)
-          self.onClickFunction(evnt);
+        if (self.onClickFunction) self.onClickFunction( evnt );
       });
     }
 
     this.init();
   }
 
-  /*
-   * Main popapilus class
-   */
-  function Popapilus(options) {
+  /** Main worker**/
+  function Popapilus( options ) {
     var self = this;
 
     var overlay = null;
     var holder = null;
 
     var _is_visible = false;
+    var _session_opts = {};
 
-    this.__defineGetter__("is_visible", function() { return _is_visible; });
+    this.__defineGetter__("isVisible", function() { return _is_visible; });
 
-    function centerHolder(show_options) {
-      show_options = show_options || options;
-
-      // by x axis
-      holder.css('left', Math.max(0, ($(window).width() / 2) - (holder.width() / 2)).toString() + 'px' );
-
-      // by y axis if position is 'fixed'
-      if (show_options.fixed)
-        holder.css('top', Math.max(0, ($(window).height() / 2) - (holder.height() / 2)).toString() + 'px' );
-    }
-
-
-    function setHolderStyles(show_options) {
-      show_options = show_options || options;
-
-      // clear old styles
-      holder.attr('style', 'display: none;');
+    function setHolderStyles() {
+      var opts = _session_opts;
+      holder.attr('style', 'display: none;'); // flush old styles
 
       holder.css({
-        position: show_options.fixed ? 'fixed' : 'absolute',
-        'z-index': show_options.z_index
+        position: opts.fixed ? 'fixed' : 'absolute',
+        'z-index': opts.z_index
       });
 
-      if (show_options.width !== null) holder.css('width', show_options.width);
-      if (show_options.height !== null) holder.css('height', show_options.height);
+      if (opts.width !== null)  holder.css('width',  opts.width);
+      if (opts.height !== null) holder.css('height', opts.height);
 
-      if (!show_options.centered) {
-        if (show_options.top !== null) holder.css('top', show_options.top);
-        if (show_options.bottom !== null) holder.css('bottom', show_options.bottom);
+      if (!(opts.centered && opts.fixed)) {
+        if (opts.top !== null)    holder.css('top',    opts.top);
+        if (opts.bottom !== null) holder.css('bottom', opts.bottom);
       }
-      if (!show_options.centered || (show_options.centered && !show_options.fixed)) {
-        if (show_options.right !== null) holder.css('right', show_options.right);
-        if (show_options.left !== null) holder.css('left', show_options.left);
+
+      if (!opts.centered) {
+        if (opts.right !== null) holder.css('right', opts.right);
+        if (opts.left !== null)  holder.css('left',  opts.left);
       }
+
+      if (holder.css('top') === "auto") { holder.css('top', 0); }
     }
 
-    function refreshPopapilus() {
-      $(window).unbind('resize');
+    function centerHolder() {
+      holder.css('left', Math.max(0, ($(window).width() / 2) - (holder.width() / 2)).toString() + 'px' ); // by x axis
+
+      if (_session_opts.fixed)
+        holder.css('top', Math.max(0, ($(window).height() / 2) - (holder.height() / 2)).toString() + 'px' ); // by y axis if position is 'fixed'
+    }
+
+    function _close() {
+      self.hide();
+    }
+
+    function inactivate() {
+      $(window).off('resize', centerHolder);
       if (overlay) overlay.onClickFunction = null;
+      holder.find(".close").off('click', _close);
     }
 
-    /*
-     * Show popapilus
-     */
-    this.show = function(data, show_options) {
-      show_options = jQuery.extend(options, show_options);
-      if (_is_visible) refreshPopapilus();
 
-      if (data) {
-        holder.html(data);
+    this.show = function( data, show_options ) {
+      _session_opts = $.extend( {}, options, show_options );
+
+      if (_is_visible) {  inactivate(); }
+
+      if (data) { holder.empty(); holder.append(data); }
+
+      if (!_session_opts.ignore_close_btn) {
+        holder.find(".close").on('click', _close);
       }
 
-      setHolderStyles(show_options);
+      setHolderStyles();
 
-      if (show_options.close_on_overlay_click && overlay) {
-        overlay.onClickFunction = function(evnt) {
-          self.hide();
-        }
+      if (_session_opts.close_on_overlay_click && overlay) {
+        overlay.onClickFunction = function(evnt) { self.hide(); }
       }
 
-      if (show_options.modal && overlay) {
-        overlay.show(show_options);
+      if (_session_opts.modal && overlay) { overlay.show(_session_opts); }
+
+      if (_session_opts.centered) {
+        $(window).on('resize', centerHolder);
+        centerHolder();
       }
 
-      if (show_options.centered) {
-        $(window).resize(function() { centerHolder(show_options); });
-        centerHolder(show_options);
-      }
-
-      if (!_is_visible)
-        holder.fadeIn(show_options.show_animation_speed);
+      if (!_is_visible) { holder.fadeIn(_session_opts.show_animation_speed); }
 
       _is_visible = true;
 
-      if (show_options.autoclose_time) {
-        setTimeout(function() {
-          self.hide();
-        }, show_options.autoclose_time);
+      if (_session_opts.autoclose_time) {
+        setTimeout(function() { self.hide(); }, _session_opts.autoclose_time);
       }
     }
 
-    /*
-     * Hide popapilus
-     */
+
     this.hide = function() {
-      refreshPopapilus();
+      inactivate();
       holder.hide();
       if (overlay) overlay.hide();
       _is_visible = false;
+      _session_option = options;
     }
 
     /* 
      * Initialize function
      */
-    this.init = function() {
-      if (!options.no_overlay) {
-        overlay = new PopapilusOverlay(options);
-      }
+    function _init() {
+      if (!options.no_overlay) { overlay = new PopapilusOverlay(options); }
       
-      // create popapilus block
+      // create a popapilus DOM block
       $("body").append('<div class="' + options.css_class + '"></div>');
       holder = $("body ." + options.css_class + ":last");
+
+      self.hide();
       setHolderStyles();
     }
 
     // rock'n'roll
-    this.init();
+    _init();
   }
 
-  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-  jQuery.popapilus = function(options) {
-    var options = jQuery.extend({
-      // overlay options
-      // * = only for init, not to show
-      no_overlay:                   false, // *
-      overlay_css_class:            'popapilus_overlay', // *
-      overlay_z_index:              3998, // *
-      overlay_show_animation_speed: 0,
-      close_on_overlay_click:       true,
-
-      // class
-      css_class: 'popapilus', // *
-
-      // position and size options
-      fixed:    true,  // 'fixed' or 'absolute'
-      z_index:  3999,
-      modal:    true,  // work only with no_overlay = false
-      centered: true,  // always work by x axis and work by y axis if fixed = true
-      top:      null,  // work with centered = false or centered = true and fixed = false
-      right:    null,  // work with centered = false
-      bottom:   null,  // work with centered = false or centered = true and fixed = false
-      left:     null,  // work with centered = false
-      width:    null,
-      height:   null,
-
-      // animation
-      show_animation_speed: 0,
-
-      // autoclose
-      autoclose_time: 0
-    }, options);
-
-    return new Popapilus(options);
-  };
-
-}).call(this);
+}( jQuery, document, window ));
